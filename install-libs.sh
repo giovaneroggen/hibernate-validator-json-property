66f2cb7e69376fa9bd28f33a6786697a71fccab0
Giovane Roggen <giovane@forttiori.com.br>
2018-11-06 16:12:35 -0200

    ajustes

--- install-libs.sh
+++ install-libs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn install:install-file -Dfile=/target/hibernate-validator-json-property-6.0.10.RELEASE.Final.jar -DpomFile=pom.xml
+mvn install:install-file -Dfile=/target/hibernate-validator-6.0.10.RELEASE.Final.jar -DpomFile=pom.xml
