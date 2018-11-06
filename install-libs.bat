d7784a586e5d6610e9eefa5604c3f88876ce4dc8
Giovane Roggen <giovane@forttiori.com.br>
2018-11-06 14:49:51 -0200

    ajuste para install de dependencias em m2 após build

--- install-libs.bat
+++ install-libs.bat
@@ -1,2 +1,2 @@
 
-call mvn install:install-file -Dfile=/target/hibernate-validator-json-property-6.0.10.Final.jar -DpomFile=pom.xml
+call mvn install:install-file -Dfile=/target/hibernate-validator-json-property-6.0.10.RELEASE.Final.jar -DpomFile=pom.xml
